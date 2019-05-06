e4bd7e54dc28732ef163ce0ded6008cdd7c2318f
Kritsana Khankaew <kridsana.khunkaew@gmail.com>
2019-05-06 17:55:00 +0700

    Status Bar

--- src/drawing/field/StatusBar.java
+++ src/drawing/field/StatusBar.java
@@ -47,13 +47,13 @@ public class StatusBar extends Field {
 	
 	@Override
 	public void draw(GraphicsContext gc) {
-		gc.drawImage(ResourceLoader.statusBar, 0, 0);
+		gc.drawImage(ResourceLoader.statusBar, 0, 550);
 		
 		gc.setFill(Color.MEDIUMSLATEBLUE);
 		gc.setFont(Font.font("Castellar",15));
-		gc.fillText(Integer.toString(((Item) Inventory.getInventory().get(0)).getAmount()), 555, 110);
-		gc.fillText(Integer.toString(((Item) Inventory.getInventory().get(1)).getAmount()), 638, 110);
-		gc.fillText(Integer.toString(((Item) Inventory.getInventory().get(3)).getAmount()), 721, 110);
+		gc.fillText(Integer.toString(((Item) Inventory.getInventory().get(0)).getAmount()), 555, 660);
+		gc.fillText(Integer.toString(((Item) Inventory.getInventory().get(1)).getAmount()), 638, 660);
+		gc.fillText(Integer.toString(((Item) Inventory.getInventory().get(3)).getAmount()), 721, 660);
 		FontLoader fontLoader = Toolkit.getToolkit().getFontLoader();
 		double fontHieght = fontLoader.getFontMetrics(gc.getFont()).getLineHeight();
 	
@@ -61,11 +61,11 @@ public class StatusBar extends Field {
 		
 		gc.setFont(Font.font("Castellar", 20));
 		double font_width = fontLoader.computeStringWidth(GameLogic.heroInBat.getName(), gc.getFont());
-		gc.fillText(GameLogic.heroInBat.getName(), 89-font_width/2, 132);
+		gc.fillText(GameLogic.heroInBat.getName(), 89-font_width/2, 682);
 		
 		//money
 		gc.setFont(Font.font("Castellar", 20));
-		gc.fillText(Double.toString(Logic.GameLogic.heroInBat.getMoney()), 855, 130);
+		gc.fillText(Double.toString(Logic.GameLogic.heroInBat.getMoney()), 855, 680);
 	
 		// Hp & Mp
 		double maxH = GameLogic.heroInBat.getMaxHp();
@@ -74,13 +74,13 @@ public class StatusBar extends Field {
 		double currentM = GameLogic.heroInBat.getCurrentMp();
 		
 		gc.setFill(Color.CRIMSON);
-		gc.fillRect(236, 47, Math.min((currentH / maxH),1) * 246, 15);
+		gc.fillRect(236, 597, Math.min((currentH / maxH),1) * 246, 15);
 
 		gc.setFill(Color.DODGERBLUE);
-		gc.fillRect(236, 89, Math.min((currentM / maxM),1) * 246, 15);
+		gc.fillRect(236, 89+550, Math.min((currentM / maxM),1) * 246, 15);
 
 
-		gc.drawImage(GameLogic.heroInBat.getAtkType().getImage(), 38, 11, 102, 100);
+		gc.drawImage(GameLogic.heroInBat.getAtkType().getImage(), 38, 561, 102, 100);
 
 	}
 	
@@ -89,20 +89,20 @@ public class StatusBar extends Field {
 		double xPos = InputUtility.mouseX;
 		double yPos = InputUtility.mouseY;
 
-		if ((xPos >= 529 && xPos <= 590 && yPos >= 30 && yPos <= 90 && InputUtility.isMouseClick())
+		if ((xPos >= 529 && xPos <= 590 && yPos >= 30+550 && yPos <= 90+550 && InputUtility.isMouseClick())
 				|| (InputUtility.isKeyPressed(KeyCode.DIGIT1))) {
 			//use HpPotion
 				inventory.use(Inventory.getInventory().get(0));
-		} else if ((xPos >= 608 && xPos <= 669 && yPos >= 30 && yPos <= 90 && InputUtility.isMouseClick())
+		} else if ((xPos >= 608 && xPos <= 669 && yPos >= 30+550 && yPos <= 90+550 && InputUtility.isMouseClick())
 				|| (InputUtility.isKeyPressed(KeyCode.DIGIT2))) {
 			//use MpPotion
 				inventory.use(Inventory.getInventory().get(1));
-		} else if ((xPos >= 691 && xPos <= 752 && yPos >= 30 && yPos <= 90 && InputUtility.isMouseClick())
+		} else if ((xPos >= 691 && xPos <= 752 && yPos >= 30+550 && yPos <= 90+550 && InputUtility.isMouseClick())
 				|| (InputUtility.isKeyPressed(KeyCode.DIGIT3))) {
 			//use MixPotion
 				inventory.use(Inventory.getInventory().get(2));
 		} else if (xPos >= Constant.SCENE_WIDTH -192 && xPos <= Constant.SCENE_WIDTH - 124 
-				&& yPos >= 25 && yPos <= 95 && InputUtility.isMouseClick()
+				&& yPos >= 25_550 && yPos <= 95+550 && InputUtility.isMouseClick()
 			|| (InputUtility.isKeyPressed(KeyCode.B))) {
 				InputUtility.mouseX = 0;
 				InputUtility.mouseY = 0;
@@ -110,7 +110,7 @@ public class StatusBar extends Field {
 				//ResourceLoader.clickSound.play(100);
 				//open shop
 		} else if (xPos >= Constant.SCENE_WIDTH -106 && xPos <= Constant.SCENE_WIDTH - 38
-				&& yPos >= 25 && yPos <= 95 && InputUtility.isMouseClick()
+				&& yPos >= 25+550 && yPos <= 95+550 && InputUtility.isMouseClick()
 			|| (InputUtility.isKeyPressed(KeyCode.ENTER))) {
 				InputUtility.mouseX = 0;
 				InputUtility.mouseY = 0;
